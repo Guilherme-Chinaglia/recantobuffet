ebaa2052b21054ceb7cc6fa982bd0bfca6a10584
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2017-10-22 05:14:31 -0200

    Inserção das imagens da geleria e dos serviços

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,10 @@ $(function(){
 		}
 	});
 
+	$('.fechar').on('click', function(){
+		$('html, body').removeClass('show-menu');
+	});
+
 	$('#js-open-menu').on('click',  function(){
 		$('html').addClass('show-menu');
 	});
@@ -18,10 +22,12 @@ function ativaScrollSuave(selector){
 
 		event.preventDefault();
 		var target = $(this).attr('href');
-
+			
 		$('html, body').animate({
 			scrollTop: $(target).offset().top
-		}, 1000)
+		}, 1000);
+
+		$('html, body').removeClass('show-menu');
 	});
 }
 
@@ -30,3 +36,16 @@ ativaScrollSuave('a[href*=quem-somos]');
 ativaScrollSuave('a[href*=servicos]');
 ativaScrollSuave('a[href*=fotos]');
 ativaScrollSuave('a[href*=contato]');
+ativaScrollSuave('a[href*=map]');
+
+//link active
+$(function(){
+	
+	var link = $('.nav-item');
+	
+	$(link).on('click', function(){
+
+		link.removeClass('active');
+		$(this).addClass('active');
+	});
+});
